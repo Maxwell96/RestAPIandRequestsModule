fb1b1f95762ccee93020f0870bae4886dedebe8c
Maxwell Agyapong <maxwell.agyapong@turntabl.io>
2023-03-01 19:04:57 +0000

    Created api_home

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your django API response"})
+
+    # This part of the code is only necessary to handle the data we are adding to our get request form the py_client
+    body = request.body # Byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # Takes in a string of JSON data and convert it -> to python dictionary
+    except:
+        pass
+
+    print(data)
+    # data["headers"] = request.headers
+    print(request.headers)
+    data["content_type"] = request.content_type
+
+    return JsonResponse(data)
--- py_client/basicTestingLocalDjango.py
+++ py_client/basicTestingLocalDjango.py
@@ -1,8 +1,12 @@
 import requests
 
 endpoint = "http://localhost:8000/api"
-get_response = requests.get(endpoint)
-print(get_response.text)
-print(get_response.status_code)
-print(get_response.json())
+# get_response = requests.get(endpoint)
+# print(get_response.text)
+# print(get_response.status_code)
+# print(get_response.json())
+# print(get_response.json()['message'])
 
+# Adding data with requests, Echo GET Data
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
+print(get_response.json())
