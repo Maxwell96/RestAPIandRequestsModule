4d32fe1ba8cfbfc97c672d3a49576d6353e02579
Maxwell Agyapong <maxwell.agyapong@turntabl.io>
2023-03-02 14:27:06 +0000

    Completed Echo GET data

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,8 @@ def api_home(request, *args, **kwargs):
 
     # This part of the code is only necessary to handle the data we are adding to our get request form the py_client
     body = request.body # Byte string of JSON data
+    print(request.GET) # To print URL query params
+    print(request.POST)  
     data = {}
     try:
         data = json.loads(body) # Takes in a string of JSON data and convert it -> to python dictionary
@@ -15,7 +17,8 @@ def api_home(request, *args, **kwargs):
     print(data)
     # data['headers'] = request.headers
     # data['headers'] = json.dupms(request.headers)
-    json.dumps(dict(request.headers))
+    # json.dumps(dict(request.headers))
+    data['params'] = dict(request.GET)
     data['headers'] = dict(request.headers)
     data['content_type'] = request.content_type
 
--- py_client/basicTestingLocalDjango.py
+++ py_client/basicTestingLocalDjango.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 # get_response = requests.get(endpoint)
 # print(get_response.text)
 # print(get_response.status_code)
